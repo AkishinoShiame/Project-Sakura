476fd7a18dc3dbe9c833237181e14fa0d962f5eb
AkishinoShiame <akishinoshiame@icloud.com>
2020-04-05 13:02:29 +0800

    update Source

---
+++ [0]DataSource/RawSource/nyaa.si_Src-Lst/FullRawList.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+for i in $(seq 1 10); do
+  DLlink="https://nyaa.si/?f=0&c=1_4&q=%22%28MX+1280x720+x264+AAC%29%22+-mp4+-mkv&s=downloads&o=desc&p=$i"
+  echo "Downloading for page $i..."
+  echo "Src Link = $DLlink =>>>"
+  wget -p -k $DLlink
+  sleep 10
+done
--- [0]DataSource/RawSource/nyaa.si_Src-Lst/list.sh
+++ [0]DataSource/RawSource/nyaa.si_Src-Lst/list.sh
@@ -4,6 +4,6 @@ for i in $(seq 1 14); do
   DLlink="https://nyaa.si/?f=0&c=1_4&q=%22%28MX+1280x720+x264+AAC%29%22+-mp4&s=downloads&o=desc&p=$i"
   echo "Downloading for page $i..."
   echo "Src Link = $DLlink =>>>"
-  wget -p -k $DLlink
+  wget -p -k $DLlink -O lndex-$i.html
   sleep 10
 done
